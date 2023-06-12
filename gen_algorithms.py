d6634de9650e3ede6f406372c0faafcfc9c52405
XoneK <mr.blackmarch@mail.ru>
2023-06-13 00:12:42 +0300

    Доделан первый ген алгоритм по Дарвину

--- gen_algorithms.py
+++ gen_algorithms.py
@@ -1,10 +1,15 @@
 import random
 from random import randrange, uniform, randint
+import numpy as np
+import networkx as nx
+from matplotlib import animation
+from matplotlib import pyplot as plt
 
 from graphs import Graph
 
 
 class DNA:
+    MAX_VALUE = 2147483647
 
     def __init__(self, length: int = None, dna=None):
         self._length = None
@@ -21,6 +26,18 @@ class DNA:
     def length(self, value):
         if value is not None:
             self._length = value
+
+    def evaluate_fitness(self, graph):
+        fitness = 0
+        for index in range(len(self.genes)):
+            if self.genes[index] == 1:
+                for j in range(index + 1, len(self.genes)):
+                    if self.genes[j] == 1 and graph.edge_adj_matrix[index][j] == 1:
+                        self.fitness = self.MAX_VALUE
+                        return
+            else:
+                fitness += 1
+        self.fitness = fitness
 
     @property
     def genes(self):
@@ -54,10 +71,30 @@ class DNA:
         return hash(tuple(self.genes))
 
 
-class GA:
-    MAX_VALUE = 2147483647
+class RecordGA:
+    def __init__(self,
+                 number_of_generations: int,
+                 fittest: list[DNA],
+                 best_fitness: int,
+                 avg_fitness: float,
+                 best_edges: list):
+        self.number_of_generations = number_of_generations
+        self.fittest = fittest
+        self.best_fitness = best_fitness
+        self.avg_fitness = avg_fitness
+        self.best_edges = best_edges
 
-    def __init__(self, graph: Graph = None):
+    def __str__(self):
+        return str(f"Номер поколения: {self.number_of_generations},\n"
+                   + f"Наилучшие особи: {self.fittest},\n"
+                   + f"Наилучшая приспособленность: {self.best_fitness} \n"
+                   + f"Средняя приспособленность поколения: {self.avg_fitness}\n"
+                   + f"Наибольшее паросочетание: {self.best_edges}\n")
+
+
+class GA:
+
+    def __init__(self, graph: Graph):
         self._generation_count = None
         self._graph = None
         self._population = []
@@ -67,9 +104,11 @@ class GA:
         self._fittest = None
 
         self.graph = graph
-        self.population_size = randrange(20, 30)
-        self.crossing_over_rate = uniform(0.8, 0.95)
-        self.mutation_rate = uniform(0.5, 1.0)
+        self.records = []
+
+        self.population_size = 800 # randrange(200, 500)
+        self.crossing_over_rate = 0.9  # uniform(0.8, 0.95)
+        self.mutation_rate = 0.9  # uniform(0.5, 1.0)
 
     @property
     def population(self):
@@ -127,69 +166,66 @@ class GA:
         if value is not None:
             self._generation_count = value
 
-    def step(self):
-        pass
+    def record_statistic(self):
+        if self.best_fitness != DNA.MAX_VALUE:
+            self.records.append(
+                RecordGA(
+                    self.generation_count,
+                    self.fittest,
+                    self.best_fitness,
+                    self.avg_fitness,
+                    [self.graph.to_edges(dna) for dna in self.fittest]
+                )
+            )
 
     def initialize_population(self):
         for _ in range(self.population_size):
             self._population.append(DNA(self.graph.number_of_edges))
         for dna in self._population:
-            dna.fitness = self.evaluate_fitness(dna)
+            dna.evaluate_fitness(self.graph)
         self.generation_count = 1
-        self.fittest = self.get_fittest()
-        self.best_fitness = self.fittest[0].fitness
-
-    def evaluate_fitness(self, dna):
-        fitness = 0
-        for i in range(len(dna.genes)):
-            if dna.genes[i] == 1:
-                for j in range(i + 1, len(dna.genes)):
-                    if dna.genes[j] == 1 and self.graph.edge_adj_matrix[i][j] == 1:
-                        return self.MAX_VALUE
-            else:
-                fitness += 1
-        return fitness
+        self.fittest, self.best_fitness = self.get_fittests()
+        self.avg_fitness = self.calculate_avg_fitness()
+        self.record_statistic()
 
     def crossover(self, parent1: DNA, parent2: DNA):
-        n = parent1.length
-        child1, child2 = DNA(n), DNA(n)
-        position = randint(2, n - 2)
+        check = uniform(0, 1)
+        if check <= self.crossing_over_rate:
+            n = parent1.length
+            child1, child2 = DNA(n), DNA(n)
+            position = randint(2, n - 2)
 
-        for i in range(position + 1):
-            child1.genes[i] = parent1.genes[i]
-            child2.genes[i] = parent2.genes[i]
+            for i in range(position + 1):
+                child1.genes[i] = parent1.genes[i]
+                child2.genes[i] = parent2.genes[i]
 
-        for i in range(position + 1, n):
-            child1.genes[i] = parent2.genes[i]
-            child2.genes[i] = parent1.genes[i]
+            for i in range(position + 1, n):
+                child1.genes[i] = parent2.genes[i]
+                child2.genes[i] = parent1.genes[i]
 
-        return child1, child2
+            return child1, child2
+        else:
+            return parent1, parent2
 
     def mutation(self, dna):
-        mutated_dna = DNA(dna.length, dna)
         check = uniform(0, 1)
         if check <= self.mutation_rate:
-            position = randint(0, len(mutated_dna.genes) - 1)
-            if mutated_dna.genes[position] == 0:
-                mutated_dna.genes[position] = 1
-            else:
-                mutated_dna.genes[position] = 0
-        return mutated_dna
+            for position in range(len(dna.genes)):
+                if dna.genes[position] == 0:
+                    dna.genes[position] = 1
+                else:
+                    dna.genes[position] = 0
+        return dna
 
     def mutation2(self, dna):
-        mutated_dna = DNA(dna.length, dna)
         check = uniform(0, 1)
-        position = randrange(0, mutated_dna.length)
+        position = randrange(0, dna.length)
         if check <= self.mutation_rate:
-            if mutated_dna.genes[position] == 0:
-                mutated_dna.genes[position] = 1
+            if dna.genes[position] == 0:
+                dna.genes[position] = 1
             else:
-                mutated_dna.genes[position] = 0
-        return mutated_dna
-
-    def choose_parents(self):
-        parents = self.roulette_wheel_selection()[0:2]
-        return parents
+                dna.genes[position] = 0
+        return dna
 
     def roulette_wheel_selection(self):
         total_fitness = 0
@@ -210,7 +246,7 @@ class GA:
                     break
         return new_population
 
-    def next_generation(self):
+    def next_generation_darvin(self):
         population_without_gen_operators = self.roulette_wheel_selection()
         new_population = []
         random.shuffle(population_without_gen_operators)
@@ -223,49 +259,108 @@ class GA:
         if len(new_population) < self.population_size:
             new_population.append(population_without_gen_operators[-1])
 
-        self.population.clear()
+        self._population = new_population
 
-        for dna in new_population:
+        for dna in self._population:
             dna = self.mutation2(dna)
-            dna.fitness = self.evaluate_fitness(dna)
-            self.population.append(dna)
+            dna.evaluate_fitness(self.graph)
 
         self.generation_count += 1
         self.avg_fitness = self.calculate_avg_fitness()
-        self.fittest = self.get_fittest()
-        self.best_fitness = self.fittest[0].fitness
+        self.fittest, self.best_fitness = self.get_fittests()
+        self.record_statistic()
 
     def calculate_avg_fitness(self):
-        return sum([dna.fitness for dna in self.population if dna.fitness != self.MAX_VALUE]) / self.population_size
+        return sum(
+            [dna.fitness for dna in self.population if dna.fitness != DNA.MAX_VALUE]) / self.population_size
 
-    def get_fittest(self):
-        min_fitness = min([i.fitness for i in self.population])
-        fittests = set([i for i in self.population if i.fitness == min_fitness])
-        return list(fittests)
+    def get_fittests(self):
+        min_fitness = min([ind.fitness for ind in self.population])
+        fittests = set([ind for ind in self.population if ind.fitness == min_fitness])
+        return list(fittests), min_fitness
 
-    def get_population_info(self):
-        return self.generation_count, self.fittest
-
-    def start(self):
-        self.initialize_population()
-        while self.best_fitness != 0 and self.generation_count != 1001:
-            if self.generation_count % 10 == 0:
-                print(
-                    f"Номер поколения: {self.generation_count},\n"
-                    f"Наилучшие особи: {self.fittest},\n"
-                    f"Наибольшее паросочетание: {[self.graph.to_edges(variant) for variant in self.fittest]}\n"
-                    f"Наилучшая приспособленность: {self.best_fitness} \n"
-                    f"Средняя приспособленность поколения: {self.avg_fitness}")
-                print("*" * 50 + "\n")
-            self.next_generation()
+    def get_best_info(self):
+        if self.records:
+            min_value = min([record.best_fitness for record in self.records])
+            bests_decisions = [record for record in self.records if record.best_fitness == min_value]
+            min_avg_value = min([decision.avg_fitness for decision in bests_decisions])
+            bests_decisions = [decision for decision in bests_decisions if decision.avg_fitness == min_avg_value]
+            return bests_decisions
         else:
-            print(
-                f"Номер поколения: {self.generation_count},\n"
-                f"Наилучшие особи: {self.fittest},\n"
-                f"Наибольшее паросочетание: {[self.graph.to_edges(variant) for variant in self.fittest]}\n"
-                f"Наилучшая приспособленность: {self.best_fitness} \n"
-                f"Средняя приспособленность поколения: {self.avg_fitness}")
-            print("*" * 50 + "\n")
+            return None
+
+    def to_file(self, path):
+        with open(path, 'w', encoding="UTF-8") as file:
+            for record in self.records:
+                file.write(
+                    f"Номер поколения: {record.number_of_generationsgeneration_count},\n"
+                    f"Наилучшие особи: {record.fittest},\n"
+                    f"Наибольшее паросочетание: {[record.graph.to_edges(variant) for variant in self.fittest]}\n"
+                    f"Наилучшая приспособленность: {record.best_fitness} \n"
+                    f"Средняя приспособленность поколения: {record.avg_fitness}\n")
+            file.write("*" * 50 + "\n")
+
+    def start_darvin(self):
+        self.initialize_population()
+
+        while self.best_fitness != 0 and self.generation_count != 1001:
+            self.next_generation_darvin()
+
+    def show_graphics(self):
+        max_fitness = [record.best_fitness for record in self.records]
+        avg_fitness = [record.avg_fitness for record in self.records]
+        plt.plot(max_fitness, color="red")
+        plt.plot(avg_fitness, color="green")
+        plt.xlabel("Поколение")
+        plt.ylabel("Макс/средняя приспособленность")
+        plt.title('Зависимость максимальной и средней приспособленности от поколения')
+        plt.show()
+
+    def show_graph(self):
+        bests = self.get_best_info()
+        if bests is None:
+            return "Решение не найдено!"
+        G = nx.Graph()
+        G.add_edges_from(self.graph.edges)
+        pair_edges = bests[0].best_edges[0]
+        edge_color_list = ["grey"] * len(G.edges)
+        for i, edge in enumerate(G.edges()):
+            if edge in pair_edges or (edge[1], edge[0]) in pair_edges:
+                edge_color_list[i] = 'red'
+        nx.draw(G, with_labels=True, edge_color=edge_color_list)
+        plt.show()
+
+    def simple_update(self, num, layout, G, ax, bests):
+        ax.clear()
+        # Draw the graph with random node colors
+        edge_color_list = ["grey"] * len(G.edges)
+        for best in bests:
+            pair_edges = best.best_edges
+            for i, edge in enumerate(G.edges()):
+                if edge in pair_edges or (edge[1], edge[0]) in pair_edges:
+                    edge_color_list[i] = 'red'
+            nx.draw(G, pos=layout, with_labels=True, edge_color=edge_color_list, ax=ax)
+
+        # Set the title
+        ax.set_title("Frame {}".format(num))
+
+    def simple_animation(self):
+        # Build plot
+        fig, ax = plt.subplots(figsize=(6, 4))
+
+        # Create a graph and layout
+        G = nx.Graph()
+        G.add_edges_from(self.graph.edges)
+        bests = self.get_best_info()
+        if bests is None:
+            return "Решение не найдено!"
+        layout = nx.spring_layout(G)
+
+        ani = animation.FuncAnimation(fig, self.simple_update, frames=20,
+                                      fargs=(layout, G, ax, bests))
+        ani.save('animation_2.gif', writer='pillow')
+
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -296,5 +391,11 @@ if __name__ == '__main__':
     ]
     # {0: [0, 1], 1: [0, 2], 2: [0, 3], 3: [1, 2], 4: [1, 4], 5: [2, 3], 6: [3, 4]}
     g = Graph(matrix=mp3)
-    ga = GA(graph=g)
-    ga.start()
+    g2 = Graph(n=13)
+    ga = GA(graph=g2)
+    ga.start_darvin()
+    ga.show_graphics()
+    bests = ga.get_best_info()
+    for i in bests:
+        print(i)
+    ga.show_graph()
--- graphs.py
+++ graphs.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def check_adj(edge_1, edge_2):
     if (edge_1[0] == edge_2[0] or edge_1[1] == edge_2[0]
             or edge_1[0] == edge_2[1] or edge_1[1] == edge_2[1]):
@@ -7,15 +10,20 @@ def check_adj(edge_1, edge_2):
 
 class Graph:
 
-    def __init__(self, matrix: list[list[int]] = None):
+    def __init__(self, matrix: list[list[int]] = None, n: int = None):
         self._edges = None
         self._vertices = None
         self._vertex_adj_matrix = None
         self._edge_adj_matrix = None
         self._edges_dict = None
-        self.set_values(matrix)
+        if matrix is None:
+            self.create_random_graph(n)
+        else:
+            self.set_values(matrix)
 
     def set_values(self, vertex_adj_matrix):
+        if vertex_adj_matrix is None:
+            return None
         self._vertices = []
         self._edges = []
         self._edges_dict = {}
@@ -25,8 +33,8 @@ class Graph:
             if v not in self._vertices:
                 self._vertices.append(v)
             for u, value in enumerate(values):
-                if value == 1 and [u, v] not in self._edges:
-                    self._edges.append([v, u])
+                if value == 1 and (u, v) not in self._edges:
+                    self._edges.append((v, u))
 
         self._edges_dict = dict(zip([i for i in range(len(self.edges))], self.edges))
         self._edge_adj_matrix = self.create_edge_adj_matrix(self.number_of_edges)
@@ -47,7 +55,7 @@ class Graph:
         edges = []
         for index, gen in enumerate(dna.genes):
             if gen == 1:
-                edges.append(self._edges_dict[index])
+                edges.append(tuple(self._edges_dict[index]))
         return edges
 
     @property
@@ -78,6 +86,21 @@ class Graph:
     def number_of_vertices(self):
         return len(self._vertices)
 
+    def create_random_graph(self, n: int):
+        graph = []
+        for i in range(n):
+            vertex = []
+            for j in range(n):
+                vertex.append(randint(0, 1))
+            graph.append(vertex)
+        for i in range(n):
+            for j in range(0, i):
+                graph[i][j] = graph[j][i]
+        for i in range(n):
+            graph[i][i] = 0
+
+        self.set_values(graph)
+
 
 if __name__ == '__main__':
     mp = [
@@ -97,8 +120,15 @@ if __name__ == '__main__':
     ]
 
     g = Graph(matrix=mp)
+    g2 = Graph(n=50)
 
-    print(g.vertices)
-    print(g.edges)
-    print(g.edges_dict)
-    print(g.edge_adj_matrix)
+    print(g2.vertex_adj_matrix)
+    with open('graph.txt', 'w') as file:
+        for vertex in g2.vertex_adj_matrix:
+            file.write(str(vertex))
+            file.write(",\n")
+
+    # print(g2.vertices)
+    # print(g2.edges)
+    # print(g2.edges_dict)
+    # print(g2.edge_adj_matrix)
