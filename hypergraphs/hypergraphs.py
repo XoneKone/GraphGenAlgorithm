2fd72a427691787c2bbea1d4f77ee407d7aba504
XoneKone <mr.blackmarch@mail.ru>
2023-12-13 01:18:16 +0300

    tochki_sochlenenya

--- hypergraphs/hypergraphs.py
+++ hypergraphs/hypergraphs.py
@@ -2,6 +2,7 @@ import random
 import random as rd
 import numpy as np
 import itertools as it
+import copy
 
 
 class HyperGraph:
@@ -28,7 +29,7 @@ class HyperGraph:
         self._vertices = value
 
     @property
-    def edges(self):
+    def edges(self) -> list[list[int]]:
         return self._edges
 
     @edges.setter
@@ -137,6 +138,75 @@ class HyperGraph:
         """
         return [value for value in self.edges[index_edge_1] if value in set(self.edges[index_edge_2])]
 
+    def find_all_intersections(self) -> list:
+        """
+        Метод для выявления всех пересечений
+        :return:
+        """
+        intersection_list = []
+        for i in range(len(self.edges) - 1):
+            for j in range(i + 1, len(self.edges)):
+                possible_intersection = self.intersection(i, j)
+                if possible_intersection and possible_intersection not in intersection_list:
+                    intersection_list.append(possible_intersection)
+        return intersection_list
+
+    def remove_vertices(self, possible_articulation_point: list[int]):
+        edges = copy.deepcopy(self.edges)
+        vertices = list(self.vertices_set)
+        for vertex in possible_articulation_point:
+            for edge in edges:
+                if vertex in edge:
+                    edge.remove(vertex)
+            if vertex in vertices:
+                vertices.remove(vertex)
+
+        return edges, vertices
+
+    def find_articulation_points(self):
+
+        intersection_list = self.find_all_intersections()
+        articulation_points = []
+
+        for possible_articulation_point in intersection_list:
+            if self.check_articulation_point(possible_articulation_point):
+                articulation_points.append(possible_articulation_point)
+        return articulation_points
+
+    def check_articulation_point(self, possible_articulation_point):
+        edges_without_possible_artic_point, vertices_without_artic_points = self.remove_vertices(
+            possible_articulation_point)
+
+        visited = {v: False for v in vertices_without_artic_points}
+
+        edge_dict = dict(
+            zip([i for i in range(len(edges_without_possible_artic_point))], edges_without_possible_artic_point))
+        adj_matrix = self.create_adj_matrix(vertices_without_artic_points, edge_dict)
+        for v in visited.keys():
+            visited = {v: False for v in vertices_without_artic_points}
+
+            self.dfs(v, visited, v, adj_matrix, edge_dict)
+            if not all(visited.values()):
+                return True
+
+        return False
+
+    def dfs(self, vertex, visited, parent, adj_matrix, edge_dict):
+        visited[vertex] = True
+
+        for adj_edge in adj_matrix[vertex]:
+            for neighbor in edge_dict[adj_edge]:
+                if not visited[neighbor] and neighbor != parent:
+                    self.dfs(neighbor, visited, vertex, adj_matrix, edge_dict)
+
+    def create_adj_matrix(self, vertices, edges_dict):
+        adj_matrix = {i: [] for i in vertices}
+        for i in vertices:
+            for j in edges_dict.keys():
+                if i in edges_dict[j]:
+                    adj_matrix[i].append(j)
+        return adj_matrix
+
     def check_all_nodes(self, genes):
         edges = set()
         for index, gen in enumerate(genes):
@@ -149,8 +219,10 @@ class HyperGraph:
 
 
 if __name__ == '__main__':
-    hg = HyperGraph(12)
-    print(hg.number_of_edges)
-    print(hg.edges)
-    print(hg.vertices)
-    print(hg.edges_dict)
+    hg = HyperGraph(9)
+    print(f"Количество ребер: {hg.number_of_edges}")
+    print(f"Ребра: {hg.edges}")
+    print(f"Вершины (разбитые на доли): {hg.vertices}")
+    print(f"Словарь ребер: {hg.edges_dict}")
+    print(f"Все возможные пересечения ребер: {hg.find_all_intersections()}")
+    print(f"Точки сочленения: {hg.find_articulation_points()}")
